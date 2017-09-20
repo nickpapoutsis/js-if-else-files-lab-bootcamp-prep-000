e44510bdf248a68ce27535d9a99c9144509fca7d
Nick Papoutsis <izybit@gmail.com>
2017-09-20 12:49:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct");
 } else {
-  console.log("Expected the name to be Susan");
+  console.log("Expected the name to be Susaniiii");
 }
 
 // Don't worry about this
